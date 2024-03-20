f8876463b0b602abcf389f6b477963b58303597c
CStryke <160301020+CStryke@users.noreply.github.com>
2024-03-20 12:47:37 -0400

    crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -34,7 +34,7 @@ var config_data = `
     { "name": "Auto Start Position",
       "code": "as",
       "type": "clickable_image",
-      "filename": "2024/field_image.png",
+      "filename": "2024/field_image_renumbered.png",
       "clickRestriction": "one",
       "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
       "shape": "circle 5 black red true"
