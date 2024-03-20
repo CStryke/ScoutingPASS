adc8a0d12d10056559dc545e0e9be2ab5a7dc090
CStryke <160301020+CStryke@users.noreply.github.com>
2024-03-20 14:01:04 -0400

    scoutingPASS.js

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -857,13 +857,13 @@ function getData(dataFormat) {
 }
 
 function updateQRHeader() {
-  let str = 'Event: !EVENT! Match: !MATCH! Robot: !ROBOT! Team: !TEAM!';
+  let str = 'Event: !EVENT! Match: !MATCH! Team: !TEAM!';
 
   if (!pitScouting) {
     str = str
       .replace('!EVENT!', document.getElementById("input_e").value)
       .replace('!MATCH!', document.getElementById("input_m").value)
-      .replace('!ROBOT!', document.getElementById("display_r").value)
+      //.replace('!ROBOT!', document.getElementById("display_r").value)
       .replace('!TEAM!', document.getElementById("input_t").value);
   } else {
     str = 'Pit Scouting - Team !TEAM!'
